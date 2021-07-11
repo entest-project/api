0cfe8e06e2c1b45bfbd025f11346143c06134773
Pierre Rolland <roll.pierre@gmail.com>
2021-07-12 01:31:55 +0200

    Throw conflict errors on duplicated feature name

--- src/Controller/CreateFeature.php
+++ src/Controller/CreateFeature.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Entity\Feature;
 use App\Repository\FeatureRepository;
 use App\Security\Voter\Verb;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -41,6 +43,8 @@ class CreateFeature extends Api
             return $this->buildSerializedResponse($feature, 'READ_FEATURE');
         } catch (ORMException | OptimisticLockException $e) {
             throw new UnprocessableEntityHttpException($e->getMessage());
+        } catch (UniqueConstraintViolationException $e) {
+            throw new ConflictHttpException();
         }
     }
 }
